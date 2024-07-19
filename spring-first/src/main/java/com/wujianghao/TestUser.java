fa676d6912abd5448f44fb2fde9aeec0196bde4c
Sclent <1140345465@qq.com>
2024-07-19 10:44:14 +0800

    日志测试

--- spring-first/src/main/java/com/wujianghao/TestUser.java
+++ spring-first/src/main/java/com/wujianghao/TestUser.java
@@ -1,13 +1,17 @@
 package com.wujianghao;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUser {
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
     @Test
     public void testUserobject() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
         User user = (User) context.getBean("user");
         user.add();
+        logger.info("### 日志手写成功");
     }}
